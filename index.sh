16f74a1167b24048c571786c2ea490cb676767d9
afeiship <1290657123@qq.com>
2019-09-17 18:23:10 +0800

    add: dns cmls

--- index.sh
+++ index.sh
@@ -2,6 +2,7 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-alias e-dns='cat /etc/resolv.conf';
+alias dns-edit='sudo vim /etc/resolv.conf';
+alias dns-list='cat /etc/resolv.conf'
 
 unset ROOT_PATH;
